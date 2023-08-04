c9af856e3544787baf98bacf69961ea5bdfeda19
Alexandre Tremblay <93389045+NoodleChad@users.noreply.github.com>
2023-08-04 12:42:46 -0400

    Update and rename Figure4a_sim.py to Figure_4a_sim.py

--- Figure_4a_sim.py
+++ Figure_4a_sim.py
@@ -28,5 +28,5 @@ while current_k >= 0.01:
     else:
         print("Infeasible!")
         break
-with open("./toronto_results/analysis_ii/analysis_II_nacl.csv", "w") as f:
+with open("./Results/Experimental/NaCl_simulated.csv", "w") as f:
             f.write(analysis_num_k)
